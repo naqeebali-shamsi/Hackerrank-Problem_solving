e4555c5a8a0be543822e75b54a34dd9cfb1d2438
Naqeebali Shamsi (C) <naqeebali.shamsi@crestdatasys.com>
2022-05-16 17:56:08 +0530

    Viral Advertising

---
+++ viral_advertising.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    # Write your code here
+    shares = 0
+    likes = 0
+    for i in range(1,n+1):
+        if i==1:
+            shares += 5
+        else:
+            shares = (shares//2) * 3
+        likes += shares//2
+    return likes     
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

9be96ab16bed532c956c810f9d1ed9e5c8a7403f
Naqeebali Shamsi (C) <naqeebali.shamsi@crestdatasys.com>
2022-05-23 13:01:04 +0530

    Halloween sale

---
+++ halloween_sale.py
@@ -0,0 +1,52 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'howManyGames' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER p
+#  2. INTEGER d
+#  3. INTEGER m
+#  4. INTEGER s
+#
+
+def howManyGames(p, d, m, s):
+    # Return the number of games you can buy
+    total_expense = 0
+    game_count = 0
+    while total_expense+p <= s:
+        total_expense += p
+        if p-d <= m:
+            p = m
+            d = 0
+        p -= d
+        game_count += 1
+        
+    return game_count
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    p = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    m = int(first_multiple_input[2])
+
+    s = int(first_multiple_input[3])
+
+    answer = howManyGames(p, d, m, s)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
